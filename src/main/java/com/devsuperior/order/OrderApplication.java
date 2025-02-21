5c2cd5a39270e9a162b9f331fbde22d47684ca58
audreyrdsc <audreyrdsc@gmail.com>
2025-02-20 23:31:11 -0300

    Ajustes na formatação da saída

--- src/main/java/com/devsuperior/order/OrderApplication.java
+++ src/main/java/com/devsuperior/order/OrderApplication.java
@@ -24,10 +24,10 @@ public class OrderApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Order order1 = new Order(1034,150.0, 20.0);
-		System.out.println("Pedido " + order1.getCode() + " Valor R$ " + String.format("%.2f", orderService.total(order1)));
+		System.out.println("\nPedido código " + order1.getCode() + " | Valor total: R$ " + String.format("%.2f", orderService.total(order1)));
 		Order order2 = new Order(2282,800.0, 10.0);
-		System.out.println("Pedido " + order2.getCode() + " Valor R$ " + String.format("%.2f", orderService.total(order2)));
+		System.out.println("Pedido código " + order2.getCode() + " | Valor total: R$ " + String.format("%.2f", orderService.total(order2)));
 		Order order3 = new Order(1309,95.90, 0.0);
-		System.out.println("Pedido " + order3.getCode() + " Valor R$ " + String.format("%.2f", orderService.total(order3)));
+		System.out.println("Pedido código " + order3.getCode() + " | Valor total: R$ " + String.format("%.2f", orderService.total(order3)) + "\n");
 	}
 }
